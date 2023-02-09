8fe22cdd98f067e808435bfc19339b01284ac72b
HAMA Barhamou <hamabarhamou@gmail.com>
2023-02-09 18:28:42 +0100

    add index.html

--- main_.py
+++ main_.py
@@ -2,27 +2,32 @@
 """
     React for python programming
 """
-from hotmetal import render
-from brouillon.component.hello import Welcome, menu, menu_item
+def main():
+    from hotmetal import render
+    from brouillon.component.hello import Welcome, menu, menu_item
 
 
-prop="ISSAKA HAMA Barhamou"
-menu_node = menu(
-    [
-        ("/home/", "Home"),
-        ("/blog/", "Blog"),
-    ]
-)
+    prop="ISSAKA HAMA Barhamou"
+    menu_node = menu(
+        [
+            ("/home/", "Home"),
+            ("/blog/", "Blog"),
+        ]
+    )
 
-app = (
-    "div",
-    {},
-    [
-        Welcome(prop),
-        menu_item("www.google.com", "faite une recherche"),
-        Welcome(prop),
-    ]
-)
-APP = render(app, indent=2)
+    app = (
+        "div",
+        {},
+        [
+            Welcome(prop),
+            menu_item("www.google.com", "faite une recherche"),
+            Welcome(prop),
+            menu_node,
+        ]
+    )
+    APP = render(app, indent=2)
 
-print(APP)
+    print(APP)
+    
+if __name__ == "__main__":
+    main()
